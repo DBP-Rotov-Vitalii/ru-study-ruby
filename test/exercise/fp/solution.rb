07b8ce7f0a5c5e90e4ea2172b3bc83e80556d0c7
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-08-25 16:58:51 -0300

    Average movie rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,11 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        interest_array = array.select do |el|
+          el['country'].to_s.split(',').count >= 2 && !el['rating_kinopoisk'].nil? && !el['rating_kinopoisk'].to_f.zero?
+        end
+        (interest_array.reduce(0) { |sum, el| sum + el['rating_kinopoisk'].to_f }) / interest_array.count
       end
 
       def chars_count(_films, _threshold)
