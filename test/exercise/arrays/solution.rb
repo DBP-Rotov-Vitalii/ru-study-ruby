fa12bf7e03aaa7da1565ce2ab7d413414c34063f
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-08-27 17:16:13 -0300

    I misunderstood the assignment at first. remade

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -5,8 +5,22 @@ module Exercise
         array.map { |el| el.positive? ? array.max : el }
       end
 
+      def bsearch_my(array, query, first, last)
+        return -1 if first >= last
+
+        half = (first + last) / 2
+        return half if array[half] == query
+
+        if array[half] < query
+          first = half + 1
+        elsif array[half] > query
+          last = half
+        end
+        bsearch_my(array, query, first, last)
+      end
+
       def search(array, query)
-        array.bsearch_index { |el| query <=> el } || -1
+        bsearch_my(array, query, 0, array.length)
       end
     end
   end
