830ed8d81a54e861ce8b8f6bab940f8dcf72715a
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-08-28 20:31:37 -0300

    my_map

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -12,7 +12,13 @@ module Exercise
       end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        my_each do |element|
+          result << yield(element)
+        end
+        result
+      end
 
       # Написать свою функцию my_compact
       def my_compact; end
