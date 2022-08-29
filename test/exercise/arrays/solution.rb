3aced1b645fe756f7d97324ed8de95dea8f78a74
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-08-29 16:24:37 -0300

    re-read the clarifications to the description of the exercise. redone without using max

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,17 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map { |el| el.positive? ? array.max : el }
+        # Немного неудобно описано задание:
+        # я сначала делал "Задачник по Ruby. Введение" целиком,
+        # потому что думал, что в следующих разделах другие задания,
+        # и запрета на использование max нет ни в его описании на DBP learn,
+        # ни в файле test.rb.
+        # Только в описании следующего раздела есть уточнение про max.
+        # Было бы лучше добавить это требование в test.rb
+        # или вообще объединить 4 раздела в 1 с общим описанием.
+        max_element = array[0]
+        array.each { |el| max_element = el if el > max_element }
+        array.map { |el| el.positive? ? max_element : el }
       end
 
       def bsearch_my(array, query, first, last)
