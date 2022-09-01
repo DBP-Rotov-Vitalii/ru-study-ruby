2f40bc28aff75b70d1cae63a6193a82ef52ec8aa
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-09-01 19:01:38 -0300

    implementation via map

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,10 +5,10 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        interest_array = array.select do |el|
-          el['country'].to_s.split(',').count >= 2 && !el['rating_kinopoisk'].nil? && !el['rating_kinopoisk'].to_f.zero?
-        end
-        (interest_array.reduce(0) { |sum, el| sum + el['rating_kinopoisk'].to_f }) / interest_array.count
+        interest_array = array.map do |el|
+          el['rating_kinopoisk'].to_f if el['country'].to_s.split(',').count >= 2 && !el['rating_kinopoisk'].to_f.zero?
+        end.compact
+        interest_array.reduce(:+) / interest_array.length
       end
 
       def chars_count(films, threshold)
