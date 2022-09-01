de4ccfa653b107a34d3006712ccfe72e6a522579
Rotov Vitalii <vitaly.rotov@dualbootpartners.com>
2022-09-01 18:23:47 -0300

    added check for an empty array and for a query exiting the array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,14 +2,6 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        # Немного неудобно описано задание:
-        # я сначала делал "Задачник по Ruby. Введение" целиком,
-        # потому что думал, что в следующих разделах другие задания,
-        # и запрета на использование max нет ни в его описании на DBP learn,
-        # ни в файле test.rb.
-        # Только в описании следующего раздела есть уточнение про max.
-        # Было бы лучше добавить это требование в test.rb
-        # или вообще объединить 4 раздела в 1 с общим описанием.
         max_element = array[0]
         array.each { |el| max_element = el if el > max_element }
         array.map { |el| el.positive? ? max_element : el }
@@ -30,6 +22,8 @@ module Exercise
       end
 
       def search(array, query)
+        return -1 if array.empty? || array[0] > query || array[-1] < query
+
         bsearch_my(array, query, 0, array.length)
       end
     end
